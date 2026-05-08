72145e4c5b441d32e271307dcd8a25099ab16309
avordnaskella <avordnaskella@gmail.com>
2026-05-08 19:16:38 +0300

    добавление всех файлов

---
+++ CalculatorLibrary.Tests/Test1.cs
@@ -0,0 +1,46 @@
+﻿namespace CalculatorLibrary.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            CalculatorLogic otvet = new CalculatorLogic();
+
+            int rez = otvet.Add(10, 4);
+
+            Assert.AreEqual(14, rez);
+        }
+
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            CalculatorLogic otvet = new CalculatorLogic();
+
+            int rez = otvet.Subtract(10, 4);
+
+            Assert.AreEqual(6, rez);
+        }
+
+        [TestMethod]
+        public void TestMethodMylty()
+        {
+            CalculatorLogic otvet = new CalculatorLogic();
+
+            int rez = otvet.Multiply(10, 4);
+
+            Assert.AreEqual(40, rez);
+        }
+
+        [TestMethod]
+        public void TestMethodDiv()
+        {
+            CalculatorLogic otvet = new CalculatorLogic();
+
+            double rez = otvet.Divide(6, 2);
+
+            Assert.AreEqual(3, rez);
+        }
+    }
+}
---
+++ CalculatorLibrary/CalculatorLogic.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace CalculatorLibrary
+{
+   
+        public class CalculatorLogic
+        {
+            public CalculatorLogic()
+            {
+                // Вся настройка прямо в конструкторе (без проверок)
+                Trace.Listeners.Add(new ConsoleTraceListener());
+                Trace.Listeners.Add(new TextWriterTraceListener("calculator_debug.log"));
+                Trace.AutoFlush = true;
+
+                Debug.WriteLine("=== БИБЛИОТЕКА ЗАГРУЖЕНА ===");
+            }
+
+            public int Add(int a, int b)
+            {
+                Debug.WriteLine($"Add({a}, {b}) = {a + b}");
+                return a + b;
+            }
+
+            public int Subtract(int a, int b)
+            {
+                Debug.WriteLine($"Subtract({a}, {b}) = {a - b}");
+                return a - b;
+            }
+
+            public int Multiply(int a, int b)
+            {
+                Debug.WriteLine($"Multiply({a}, {b}) = {a * b}");
+                return a * b;
+            }
+
+            public double Divide(int a, int b)
+            {
+                Debug.WriteLine($"Divide({a}, {b})");
+
+                if (b == 0)
+                {
+                    Debug.WriteLine("ОШИБКА: деление на ноль!");
+                    if (Debugger.IsAttached) Debugger.Break();
+                    throw new DivideByZeroException("Нельзя делить на ноль!");
+                }
+
+                return (double)a / b;
+            }
+        }
+    }
+
+
